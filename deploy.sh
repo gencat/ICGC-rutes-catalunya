38d8e84412c3eadbbf4c06a94c79606d3a9748ae
geostarters <geostarters@gmail.com>
2020-07-08 12:59:41 +0200

    add config files

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 rm -fr /home/tic/rutes-catalunya
+
 cp -r tmp /home/tic/rutes-catalunya
 
-#service nginx stop
+# service nginx stop
 rm -fr /var/www/html/rutes-catalunya/*
-cp -r /home/tic/rutes-catalunya/*  /var/www/html/rutes-catalunya/
+mkdir -p /var/www/html/rutes-catalunya
+cp  /home/tic/rutes-catalunya/index.html  /var/www/html/rutes-catalunya/
+cp  /home/tic/rutes-catalunya/favicon.ico  /var/www/html/rutes-catalunya/
+cp -r /home/tic/rutes-catalunya/dist  /var/www/html/rutes-catalunya/
 
 #service nginx start
